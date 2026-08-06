c58e475db2c3a42676f043704335c52f65533cf0
Junghwan <hwane2000@gmail.com>
2026-08-06 17:28:08 +0900

    [setting] 프로젝트 세팅

---
+++ ONEULSIK/ONEULSIK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ONEULSIK
+//
+//  Created by 박정환 on 8/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ONEULSIK/ONEULSIK/ONEULSIKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONEULSIKApp.swift
+//  ONEULSIK
+//
+//  Created by 박정환 on 8/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ONEULSIKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
